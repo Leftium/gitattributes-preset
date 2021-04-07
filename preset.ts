3c5491d1646c56f6d208ad19cc6b55356058cee0
Leftium <john@leftium.com>
2021-04-07 20:01:24 +0900

    Add actions to add .gitattributes file.
    1. Copy file.
    2. Add lock files.
    3. Add nodiff command.

--- preset.ts
+++ preset.ts
@@ -1,4 +1,23 @@
 import { Preset } from 'apply';
 
 Preset.setName('gitattributes-preset');
-Preset.extract();
+Preset.extract().whenConflict('skip');
+Preset.edit('.gitattributes').update((content, preset) => {
+    let result = content;
+
+    if (!result.includes('package-lock.json')) {
+        result += `\npackage-lock.json   diff=nodiff`;
+    }
+
+    if (!result.includes('yarn.lock')) {
+        result += `\nyarn.lock           diff=nodiff`;
+    }
+
+    if (!result.includes('pnpm-lock.yaml')) {
+        result += `\npnpm-lock.yaml      diff=nodiff`;
+    }
+
+    return result;
+});
+Preset.execute('git', 'config', 'diff.nodiff.command', 'f () { echo \"Diff skipped: $1\"; }; f $1');
+
